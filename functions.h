3f6703772d9cabe963caf39e254534c9d5e36d01
GiovanniNeve <thomasz01@outlook.it>
2020-08-21 11:00:29 +0200

    Better performance and added comments

---
+++ functions.h
@@ -0,0 +1,65 @@
+#ifndef ANAGRAMMI_CREATOR_FUNCTIONS_H
+#define ANAGRAMMI_CREATOR_FUNCTIONS_H
+
+#endif //ANAGRAMMI_CREATOR_FUNCTIONS_H
+
+/* Find duplicates of a word */
+int findDuplicates(std::string word) {
+    int numberOfDubplicates = 0;
+    int strLen = word.length();     /* Get the word length */
+    std::map<char, int> map;    /* Store word's letter in a map */
+
+    for(int i=0; i<strLen; i++) {
+        map[word[i]] += 1;  /* If there are duplicates some letters will result higher than 1 */
+    }
+
+    for(auto element : map) {
+        if(element.second > 1) {
+            numberOfDubplicates += element.second;  /* Increment the sum of duplicates */
+        }
+    }
+
+    return numberOfDubplicates;
+}
+
+/* Factorial number function */
+int findFactorial(int number, int result) {     /* Calculate factorial number with recursion*/
+    return (number == 0) ? result : findFactorial(number - 1, result * number);
+}
+
+
+/* Create anagrams of a word  */
+std::vector<std::string> makeAnagrams(std::string word) {
+    std::vector<std::string> anagramsArray;     /* Create an empty string array to store anagrams */
+    std::string tempWord = word;    /* Create a copy of the word to work with */
+    int wordLen = word.length();    /*Get word length */
+    int nDuplicates = findDuplicates(word);     /* Store the number of duplicates */
+
+    /* Print number of possible anagrams */
+    /* Use the permutation formula for duplicates */
+    std::cout << "\nNumber of possible anagrams: " << (findFactorial(wordLen,1) / findFactorial(nDuplicates, 1)) << "\n";
+
+    std::cout << "Making anagrams...\n";
+    std::sort(tempWord.begin(), tempWord.end());    /* Sort the letter of the word */
+    anagramsArray.emplace_back(tempWord);   /* Insert the first word as an anagram */
+
+    while(std::next_permutation(tempWord.begin(), tempWord.end())) {    /* Create permutation with next_permutation formula */
+        anagramsArray.emplace_back(tempWord);   /* Insert the other anagrams */
+    }
+
+    return anagramsArray;
+}
+
+/* Search the word inserted in the dictionary */
+void search(std::vector<std::string> anagramsArray, std::shared_ptr<std::vector<std::string>> dataArray) {
+    for(auto word : *dataArray) {
+        for (auto wordOfArray : anagramsArray) {
+            /* If the first letter of the inserted word is ahead it will skip all of them */
+            if (wordOfArray[0] < word[0]) {
+                continue;
+            } else if(word.compare(wordOfArray) == 0) {     /* If the words are the same it prints it out */
+                std::cout << "\nWord found " << word<< "\n";
+            }
+        }
+    }
+}
--- mainFile.cpp
+++ mainFile.cpp
@@ -5,60 +5,7 @@
 #include <cstring>
 #include <map>
 #include <algorithm>
-#include "./binarySearchTree.h"
-
-/* Find duplicates in a word for the permutation formula */
-int findDuplicates(std::string word) {
-    int numberOfDubplicates = 0;
-    int strLen = word.length();
-    std::map<char, int> map;    /* Store word's letter in a map */
-
-    for(int i=0; i<strLen; i++) {
-        map[word[i]] += 1;  /* If there are duplicates some words will result higher than 1 */
-    }
-
-    for(auto element : map) {
-        if(element.second > 1) {
-            numberOfDubplicates += element.second;  /* Increment the sum of duplicates */
-        }
-    }
-
-    return numberOfDubplicates;
-}
-
-/* Factorial number function */
-int findFactorial(int number, int result) {     /* Calculate factorial number for the permutation formula with recursion*/
-    return (number == 0) ? result : findFactorial(number-1, result*number);
-}
-
-/* Create anagrams and search them in the BST */
-std::vector<std::string> makeAnagrams(std::string word) {
-    std::vector<std::string> anagramsArray;
-    std::string tempWord = word;
-    int wordLen = word.length();    /*Get word length */
-    int nDuplicates = findDuplicates(word);     /* Get number of duplicates */
-
-    /* Print number of possible anagrams with duplicates permutation formula */
-    std::cout << "\nNumber of possible anagrams: " << (findFactorial(wordLen,1)/findFactorial(nDuplicates, 1)) << "\n";
-
-    std::cout << "Making anagrams...\n";
-    std::sort(tempWord.begin(), tempWord.end());    /* Sort the word */
-    anagramsArray.emplace_back(tempWord);
-
-    while(std::next_permutation(tempWord.begin(), tempWord.end())) {    /* Create permutation with algorithm.h funcion */
-        anagramsArray.emplace_back(tempWord);
-    }
-
-    return anagramsArray;
-}
-
-void search(std::vector<std::string> anagramsArray, std::shared_ptr<BST> bst)
-{
-    std::cout << "Searching...\n";
-    for (auto wordOfArray : anagramsArray) {
-        bst->search(wordOfArray, bst->root);    //Search data in the BST
-    }
-}
+#include "functions.h"
 
 int main() {
 
@@ -66,38 +13,49 @@ int main() {
     std::string output;
     std::string word;
     std::vector<std::string> arrayOfAnagrams;
+    int fileLength;
 
     file.open("../wordList.txt", std::ios::in | std::ios::out);   /* Try to open the file */
     if(file.fail()) {   /* Check for errors */
-        std::cout << "Errore nell'apertura del file, file non trovato o corrotto...";
+        std::cout << "Error occurred, file not found or corrupted...";
         return 1;
     }else {
-        std::cout << "File aperto correttamente\n";
+        std::cout << "File opened correctly\n";
     }
 
-        std::cout << "--------------------\n";
-        std::cout << "Inserisci una parola: ";
-        std::cin >> word;
-        if (word=="exit") {    /* Check for break */
-            return 0;
+    file.seekg(0, file.end);
+    fileLength = file.tellg();
+    file.seekg(0, file.beg);
+
+    std::cout << "--------------------\n";
+    std::cout << "Insert the word: ";
+    std::cin >> word;
+    if (word=="exit") {    /* Check for exit */
+        return 0;
+    }
+
+    /* Make anagrams of the word inserted */
+    arrayOfAnagrams = makeAnagrams(word);
+
+    /* Read the file 131 KB at once (around 11 hundred words at once) */
+    while(file) {
+        int filePosition = file.tellg();    /* Get iterator position for percentage */
+        std::cout << "\n" << (filePosition*100)/fileLength << "%";  /* Calculate and print percentage */
+        {   /* Start shared pointer */
+        /* Create a vector pointer */
+        std::shared_ptr<std::vector<std::string>> dataArray(new std::vector<std::string>);
+
+        constexpr size_t bufferSizeLimit = 1024 * 128;
+        size_t bufferSize = 0;
+
+        /* Load block of data in the vector */
+        while (file >> output && bufferSize < bufferSizeLimit) {
+            dataArray->emplace_back(output);
+            bufferSize += sizeof(output);
         }
 
-        arrayOfAnagrams = makeAnagrams(word);
-
-        /* Lettura del file e inserimento dei dati nel binary search tree*/
-        while(file) {
-            {   /* Start shared pointer */
-            std::shared_ptr<BST> bst(new BST);  /* BST is the binary search tree class */
-
-            constexpr size_t bufferSizeLimit = 1024 * 128;
-            size_t bufferSize = 0;
-
-            while (file >> output && bufferSize < bufferSizeLimit) {
-                bst->insertData(output, bst->root);
-                bufferSize += sizeof(output);
-            }
-
-            search(arrayOfAnagrams, bst);
+        /* Search word in the vector */
+        search(arrayOfAnagrams, dataArray);
 
         }   /*End shared pointer*/
     }
