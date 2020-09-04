7e7dbf37d73b4c7b6868226dc797770a69039f39
GiovanniNeve <thomasz01@outlook.it>
2020-09-04 19:39:47 +0200

    Better performance

--- mainFile.cpp
+++ mainFile.cpp
@@ -16,54 +16,68 @@ int main() {
     std::vector<std::string> foundAnagrams;
     int fileLength;
 
-    file.open("../wordList.txt", std::ios::in | std::ios::out);   // Try to open the file
-    if(file.fail()) {   // Check for errors
-        std::cout << "Error occurred, file not found or corrupted...";
-        return 1;
-    }else {
-        std::cout << "File opened correctly\n";
-    }
+    while(true) {
 
-    file.seekg(0, file.end);
-    fileLength = file.tellg();
-    file.seekg(0, file.beg);
-
-    std::cout << "--------------------\n";
-    std::cout << "Insert the word: ";
-    std::cin >> word;
-    if (word=="exit") {    // Check for exit
-        return 0;
-    }
-
-    // Make anagrams of the word inserted
-    arrayOfAnagrams = makeAnagrams(word);
-
-    // Read the file 131 KB at once (around 11 hundred words at once)
-    while(file) {
-        int fileIteratorPosition = file.tellg();    // Get iterator position for percentage
-        std::cout << std::endl << (fileIteratorPosition*100)/fileLength << "%";  // Calculate and print percentage
-        {   // Start shared pointer
-        // Create a vector pointer
-        std::shared_ptr<std::vector<std::string>> dataArray(new std::vector<std::string>);
-
-        constexpr size_t bufferSizeLimit = 1024 * 128;
-        size_t bufferSize = 0;
-
-        // Load block of data in the vector
-        while (file >> fileOutput && bufferSize < bufferSizeLimit) {
-            dataArray->emplace_back(fileOutput);
-            bufferSize += sizeof(fileOutput);
+        file.open("../wordList.txt", std::ios::in | std::ios::out);   // Try to open the file
+        if(file.fail()) {   // Check for errors
+            std::cout << "Error occurred, file not found or corrupted...";
+            return 1;
         }
 
-        // Search word in the vector
-        auto tempAnagrams = search(arrayOfAnagrams, dataArray);
-        foundAnagrams.insert(foundAnagrams.end(), tempAnagrams.begin(), tempAnagrams.end());
+        file.seekg(0, file.end);
+        fileLength = file.tellg();
+        file.seekg(0, file.beg);
 
-        }   //End shared pointer
-    }
-    std::cout << "\n\nAnagrams found:";
-    for(auto element : foundAnagrams) {
-        std::cout << std::endl << element;
+        std::cout << "--------------------\n";
+        std::cout << "Insert the word: ";
+        std::cin >> word;
+        if (word=="exit") {    // Check for exit
+            return 0;
+        }
+
+        // Make anagrams of the word inserted
+        arrayOfAnagrams = makeAnagrams(word);
+
+        // Read the file 131 KB at once (around 11 hundred words at once)
+        while (file) {
+            int fileIteratorPosition = file.tellg();    // Get iterator position for percentage
+            std::cout << std::endl << (fileIteratorPosition*100)/fileLength << "%";  // Calculate and print percentage
+            {   // Start shared pointer
+                // Create a vector pointer
+                std::shared_ptr<std::vector<std::string>> dataArray(new std::vector<std::string>);
+
+                constexpr size_t bufferSizeLimit = 1024*128;
+                size_t bufferSize = 0;
+
+                // Load block of data in the vector
+                while (file >> fileOutput && bufferSize<bufferSizeLimit) {
+                    dataArray->emplace_back(fileOutput);
+                    bufferSize += sizeof(fileOutput);
+                }
+
+                // Search word in the vector
+                auto tempAnagrams = search(arrayOfAnagrams, dataArray);
+                foundAnagrams.insert(foundAnagrams.end(), tempAnagrams.begin(), tempAnagrams.end());
+
+            }   //End shared pointer
+        }
+        //Reset file index
+        file.close();
+
+        std::cout << "\n\nAnagrams found:";
+        for (auto element : foundAnagrams) {
+            std::cout << std::endl << element;
+        }
+
+        //Empty the 2 vectors
+        foundAnagrams.clear();
+        arrayOfAnagrams.clear();
+
+        char choose;
+        std::cout << "\nRedo? (y/n): ";
+        std::cin >> choose;
+        if (choose=='n' || choose=='N')
+            break;
     }
 
 	return 0;
